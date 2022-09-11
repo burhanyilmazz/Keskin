fc10f0cb68f31119d2145c9435c9a22625c21eb4
Burhan Yılmaz <burhan.yilmaz@consultant.turkcell.com.tr>
2022-09-12 00:30:41 +0300

    contact

--- pages/[locale]/contact.js
+++ pages/[locale]/contact.js
@@ -1,20 +1,164 @@
 /* eslint-disable jsx-a11y/alt-text */
+import {useEffect, useRef} from 'react'
 import Head from 'next/head'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next';
 import i18nextConfig from '../../next-i18next.config'
 import { getI18nPaths } from '../../getI18nPaths'
+import classNames from 'classnames';
+
+import {Loader} from '@googlemaps/js-api-loader';
+
 import { Layout } from '../../layout'
+import { ContactCard } from '../../components/';
 
 import styles from '../../assets/styles/Contact.module.scss'
 
 import { products } from '../../utils/Products';
-import Image from 'next/image'
-import classNames from 'classnames';
-import { ContactCard } from '../../components/';
 
 export default function Contact() {
   const { t } = useTranslation('common');
+
+  const googlemap = useRef(null);
+
+  useEffect(() => {
+    const loader = new Loader({
+      client: "gme-turkcell",
+      channel: "TURKCELLCOMTR",
+      sensor: false,
+      language: 'tr'
+    });
+    let map;
+
+    loader.load().then(() => {
+      const google = window.google;
+      map = new google.maps.Map(googlemap.current, {
+        center: {lat: 36.9638755, lng: 28.6848109},
+        zoom: 12,
+        fullscreenControl: false, 
+        mapTypeControl: false,
+        streetViewControl: false,
+        zoomControl: false,
+        styles: [{
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#e9e9e9"
+          }, {
+            "lightness": 17
+          }]
+        }, {
+          "featureType": "landscape",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#f5f5f5"
+          }, {
+            "lightness": 20
+          }]
+        }, {
+          "featureType": "road.highway",
+          "elementType": "geometry.fill",
+          "stylers": [{
+            "color": "#ffffff"
+          }, {
+            "lightness": 17
+          }]
+        }, {
+          "featureType": "road.highway",
+          "elementType": "geometry.stroke",
+          "stylers": [{
+            "color": "#ffffff"
+          }, {
+            "lightness": 29
+          }, {
+            "weight": 0.2
+          }]
+        }, {
+          "featureType": "road.arterial",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#ffffff"
+          }, {
+            "lightness": 18
+          }]
+        }, {
+          "featureType": "road.local",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#ffffff"
+          }, {
+            "lightness": 16
+          }]
+        }, {
+          "featureType": "poi",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#f5f5f5"
+          }, {
+            "lightness": 21
+          }]
+        }, {
+          "featureType": "poi.park",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#dedede"
+          }, {
+            "lightness": 21
+          }]
+        }, {
+          "elementType": "labels.text.stroke",
+          "stylers": [{
+            "visibility": "on"
+          }, {
+            "color": "#ffffff"
+          }, {
+            "lightness": 16
+          }]
+        }, {
+          "elementType": "labels.text.fill",
+          "stylers": [{
+            "saturation": 36
+          }, {
+            "color": "#333333"
+          }, {
+            "lightness": 40
+          }]
+        }, {
+          "elementType": "labels.icon",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "transit",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#f2f2f2"
+          }, {
+            "lightness": 19
+          }]
+        }, {
+          "featureType": "administrative",
+          "elementType": "geometry.fill",
+          "stylers": [{
+            "color": "#fefefe"
+          }, {
+            "lightness": 20
+          }]
+        }, {
+          "featureType": "administrative",
+          "elementType": "geometry.stroke",
+          "stylers": [{
+            "color": "#fefefe"
+          }, {
+            "lightness": 17
+          }, {
+            "weight": 1.2
+          }]
+        }]
+      });
+    });
+  });
+
 
   return (
     <>
@@ -24,6 +168,7 @@ export default function Contact() {
       </Head>
       
       <Layout products={products}>
+        <div id="map" ref={googlemap} />
         <div className='content'>
           <div className={classNames('container', styles['contact'])}>
             <div className={styles['list']}>
