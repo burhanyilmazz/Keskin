c1a355b57df0e7b1081c7221c8cfc3f205b689f2
Burhan Yılmaz <burhan.yilmaz@consultant.turkcell.com.tr>
2022-10-13 20:53:45 +0300

    search

--- components/Search/index.js
+++ components/Search/index.js
@@ -1,32 +1,57 @@
-import PropTypes from 'prop-types';
+import {useState} from 'react';
+import {useRouter} from 'next/router'
 import { useTranslation } from 'next-i18next';
+import classNames from 'classnames';
+
+import * as Yup from 'yup'
+import {useFormik} from 'formik'
 
 import styles from './Search.module.scss';
 import { FormInput, Icon } from '../';
 
 
-export const Search = (props) => {
-  const { isShow } = props;
+export const Search = () => {
+  const router = useRouter()
+  const { t, i18n } = useTranslation('common');
 
-  const { t } = useTranslation('common')
+  const searchSchema = Yup.object().shape({
+    keyword: Yup.string().required(t('VALIDATION.GENERAL')),
+  })
+
+  const [search] = useState({
+    keyword: '',
+  })
+
+  const formik = useFormik({
+    initialValues: search,
+    validationSchema: searchSchema,
+    onSubmit: async (values, {setSubmitting}) => {
+      setSubmitting(true);
+      const pathname = `/${i18n.language}/search`
+      router.push({
+        pathname,
+        query: {keyword: values.keyword},
+      })
+    },
+  })
 
   return (
     <div className={styles['search']}>
-      <form>
+      <form onSubmit={formik.handleSubmit} noValidate>
         <div className={styles['input']}>
           <div className={styles['input-icon']}><Icon icon={'search'} /></div>
-          <FormInput field={t('SEARCH.BAR.INPUT')} className={styles['search-input']} name={'search'} />
-          <div className={styles['input-submit']}><Icon icon={'enter'} /></div>
+          <FormInput 
+            field={t('SEARCH.BAR.INPUT')} 
+            name={'keyword'} 
+            type="text" 
+            required={true}
+            errorMessage={formik.errors.keyword}
+            className={classNames(styles['search-input'], {'is-invalid': formik.touched.keyword && formik.errors.keyword})}
+            {...formik.getFieldProps('keyword')}
+          />
+          <button type='submit' className={styles['input-submit']}><Icon icon={'enter'} /></button>
         </div>
       </form>
     </div>
   )
-}
-
-Search.propTypes = {
-	isShow: PropTypes.bool,
-};
-
-Search.defaultProps = {
-	isShow: false,
 }
--- pages/[locale]/search.js
+++ pages/[locale]/search.js
@@ -1,19 +1,42 @@
 /* eslint-disable jsx-a11y/alt-text */
+import { useState, useEffect } from 'react';
 import Head from 'next/head'
+import {useRouter} from 'next/router'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next';
 import i18nextConfig from '../../next-i18next.config'
 import { getI18nPaths } from '../../getI18nPaths'
 import { Layout } from '../../layout'
 import fetch from 'isomorphic-unfetch'
+import {API_URL} from '../../utils/env'
 
 import styles from '../../assets/styles/Search.module.scss'
 
-import { blogs } from '../../utils/Blog';
 import { TopBar, Search, Card } from '../../components';
 
 export default function SearchPage({products}) {
-  const { t } = useTranslation('common');
+  const [data, setData] = useState();
+  const { t, i18n } = useTranslation('common');
+  const router = useRouter();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search)
+    const language = i18n.language;
+    const keyword = router.query.keyword || params.get('keyword');
+
+    if (!keyword) return
+
+    fetch(`${API_URL}/search`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({language, keyword})
+    }).then(r => r.json()).then(data => setData(data.Result));
+  }, [])
+  
+  const blogUrl = i18n.language === 'tr' ? '/blog-detay' : '/blog-detail';
+  const productUrl = i18n.language === 'tr' ? '/urun-detay' : '/product-detail';
 
   return (
     <>
@@ -35,29 +58,29 @@ export default function SearchPage({products}) {
             </div>
           </div>
 
-          <div className={styles['search-list']}>
+          {data?.products && <div className={styles['search-list']}>
             <div className='container'>
               <div className='min-title'>{t('HOME.PRODUCTS.TITLE')}</div>
 
               <div className={styles['product-list']}>
                 {
-                  products.map((item, index) => <div key={index} className={styles['products__item']}><Card title={item.title} href={'/product-detail'} /></div> )
+                  data.products.map((item, index) => <div key={index} className={styles['products__item']}><Card title={item.title} href={'/product-detail'} /></div> )
                 }
               </div>
             </div>
-          </div>
+          </div> }
 
-          <div className={styles['search-list']}>
+          {data?.blogs && <div className={styles['search-list']}>
             <div className='container'>
               <div className='min-title'>{t('SIDEBAR.BLOG')}</div>
 
               <div className={styles['blog-list']}>
                 {
-                  blogs.map((item, index) => <div key={index} className={styles['products__item']}> <Card title={item.title}  desc={item.description}  href={'/blog-detail'}  src={item.images.medium} /></div>)
+                  data.blogs.map((item, index) => <div key={index} className={styles['products__item']}> <Card title={item.title} desc={item.description}  href={'/blog-detail'} src={item.listing} /></div>)
                 }
               </div>
             </div>
-          </div>
+          </div> }
             
          </div>
         </div>
