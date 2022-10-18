f1f695821c37be7dde9bdcb23118c99ac90dc9af
Burhan Yılmaz <burhan.yilmaz@consultant.turkcell.com.tr>
2022-10-18 22:30:35 +0300

    deneme

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    await res.revalidate(req.query.path);
+    await res.revalidate('/tr/bayilikler');
     return res.json({
       revalidated: true
     });
